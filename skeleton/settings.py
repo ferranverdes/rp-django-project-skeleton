80823e89cd67719329c3051b95618bbaf2291bd3
Ferran Verdés <ferranverdes@gmail.com>
2020-02-15 15:24:37 +0100

    Added Token Authentication

--- skeleton/settings.py
+++ skeleton/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'rest_framework',
+	'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Token Authentication
+REST_FRAMEWORK = {
+	'DEFAULT_AUTHENTICATION_CLASSES': [
+		'rest_framework.authentication.TokenAuthentication'
+	],
+}
