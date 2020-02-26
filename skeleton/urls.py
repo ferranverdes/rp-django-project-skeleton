8a0ced459e26ee386952ad155b20bf0981fb27ec
Ferran Verdés <ferranverdes@gmail.com>
2020-02-26 19:02:12 +0100

    Added collectstatic command on Travis CI configuration

--- skeleton/urls.py
+++ skeleton/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('token-auth/', obtain_auth_token, name='token_auth'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
